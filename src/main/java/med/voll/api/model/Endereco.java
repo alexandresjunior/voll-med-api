11fd76db439a05a1835add6739214443c440d0f0
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2023-05-05 16:18:18 -0300

    Criação das entidades JPA: Medico e Endereco (embeddable)

---
+++ src/main/java/med/voll/api/model/Endereco.java
@@ -0,0 +1,22 @@
+package med.voll.api.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+
+}
---
+++ src/main/java/med/voll/api/model/Medico.java
@@ -0,0 +1,38 @@
+package med.voll.api.model;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import med.voll.api.constants.Especialidade;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "tb_medicos")
+@Entity(name = "Medico")
+public class Medico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+
+}
