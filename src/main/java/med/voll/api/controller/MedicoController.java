4d6662e7ac13ec5eec839252d2127e7ea3fdfafe
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2023-05-08 15:06:05 -0300

    Exclusão (definitiva) de médico do banco

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,7 +8,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -46,6 +48,12 @@ public class MedicoController {
         if (medico.isPresent()) {
             medico.get().updateModelFromDTO(medicoDTO);
         }
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluirMedico(@PathVariable("id") Long id) {
+        medicoRepository.deleteById(id);
     }
 
     @Autowired
