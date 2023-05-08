ad64f0b675d388856b4d926039f4c465611724f9
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2023-05-08 15:39:42 -0300

    Flyway migration para tabela de pacientes

---
+++ src/main/resources/db/migration/V4__CREATE-TABLE-PACIENTES.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS tb_pacientes (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    cpf VARCHAR(20) NOT NULL UNIQUE,
+    logradouro VARCHAR(100) NOT NULL,
+    bairro VARCHAR(100) NOT NULL,
+    cep VARCHAR(9) NOT NULL,
+    complemento VARCHAR(100),
+    numero VARCHAR(20),
+    uf CHAR(2) NOT NULL,
+    cidade VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id)
+)
